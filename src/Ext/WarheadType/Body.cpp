46822c8e04d3b0c070f9a59964b6005ee892c875
Noble_Fish <1065703286@qq.com>
2026-06-13 17:29:00 +0800

    Revert "update"
    This reverts commit 664d702478cd3eb5831ecfcb85bd170545ae9374.

--- src/Ext/WarheadType/Body.cpp
+++ src/Ext/WarheadType/Body.cpp
@@ -86,7 +86,8 @@ bool WarheadTypeExt::ExtData::IsInvokerAllowed(TechnoClass* pTarget, TechnoClass
 	}
 	else
 	{
-		return !this->AffectsInvokerOnly_IgnoreInvokerState.Get(RulesExt::Global()->AffectsInvokerOnly_IgnoreInvokerState);
+		return !this->AffectsInvokerOnly_IgnoreInvokerState.Get(
+			RulesExt::Global()->AffectsInvokerOnly_IgnoreInvokerState);
 	}
 }
 
